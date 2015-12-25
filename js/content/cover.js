7a1a9085a2d26302b77f993d4581a439b37b1fb5
Leon Denise <lowpolygonum@gmail.com>
2015-12-25 20:12:12 +0100

    rearranging

--- js/content/cover.js
+++ js/content/cover.js
@@ -7,63 +7,54 @@ var Cover = function (preview, gif, link) {
 
 var coverList = {
 	'[Brain Slash Cover]': new Cover (
-    'content/BrainSlash.png',
-    'content/BrainSlash.gif'
+    'content/BrainSlash.png', 'content/BrainSlash.gif'
   ), 
   '[Ding Dong Cover]': new Cover (
-    'content/DingDong.png',
-    'content/DingDong.gif',
+    'content/DingDong.png', 'content/DingDong.gif',
     'https://www.youtube.com/embed/hzwytMMhHTk'
   ), 
   '[GPU Cover]': new Cover (
-    'content/GPU.png',
-    'content/GPU.gif',
-    'http://mechbird.fr/en/gpu.html'
+    'content/GPU.png', 'content/GPU.gif',
+    'https://player.vimeo.com/video/127556214'
   ),
   '[Gif Jam Cover]': new Cover (
-    'content/GifJam.png',
-    'content/GifJam.gif',
+    'content/GifJam.png', 'content/GifJam.gif',
     'https://www.facebook.com/gifjamm/?fref=ts'
   ),
   '[X3 Cover]': new Cover (
-    'content/X3.png',
-    'content/X3.gif',
-    'http://mechbird.fr/en/xwhydotcube.html'
+    'content/X3.png', 'content/X3.gif',
+    'https://player.vimeo.com/video/119758881'
   ),
   '[Nivose Cover]': new Cover (
-    'content/Nivose.png',
-    'content/Nivose.gif'
+    'content/Nivose.png', 'content/Nivose.gif',
+    'https://player.vimeo.com/video/84936835'
   ),
   '[Ben Kaleido Cover]': new Cover (
-    'content/BenKaleido.png',
-    'content/BenKaleido.gif',
+    'content/BenKaleido.png', 'content/BenKaleido.gif',
     'https://player.vimeo.com/video/139346593'
   ),
   '[Splashes Cover]': new Cover (
-    'content/Splashes.png',
-    'content/Splashes.gif',
+    'content/Splashes.png', 'content/Splashes.gif',
     'https://www.youtube.com/embed/LLV7s8FtUy8'
   ),
   '[Gossima VJ Cover]': new Cover (
-    'content/Gossima1.png',
-    'content/Gossima1.gif'
+    'content/Gossima1.png', 'content/Gossima1.gif'
   ),
   '[Gossima Ping Pong Cover]': new Cover (
-    'content/Gossima2.png',
-    'content/Gossima2.gif'
+    'content/Gossima2.png', 'content/Gossima2.gif'
   ),
   '[Warband Cover]': new Cover (
-    'content/Warband.png',
-    'content/Warband.gif',
+    'content/Warband.png', 'content/Warband.gif',
     'https://www.youtube.com/embed/vRf6f73LZGY'
   ),
   '[Glitch1 Cover]': new Cover (
-    'content/Glitch1.png',
-    'content/Glitch1.gif'
+    'content/Glitch1.png', 'content/Glitch1.gif'
+  ),
+  '[Glitch2 Cover]': new Cover (
+    'content/Glitch2.png', 'content/Glitch2.gif'
   ),
   '[Morrowind Cover]': new Cover (
-    'content/Morrowind.png',
-    'content/Morrowind.gif',
+    'content/Morrowind.png', 'content/Morrowind.gif',
     'https://www.youtube.com/embed/uQdgjZYbhQ4'
   )
 };
--- js/content/meta.js
+++ js/content/meta.js
@@ -8,25 +8,24 @@ var Meta = function (columnSize, rowHeight, title, cover, description, credits) 
   this.credits = credits || [];
 }
 
-var defaultHeight = 300;
+var defaultHeight = 400;
 
 var metaList = [[
 	new Meta (12, 1, '', '', '[Nanokiss Description]')
-],[
-	new Meta (4, 400, 'Gif Jam', '[Gif Jam Cover]', '[Gif Jam Description]', ['[Alexia]', '[Ben]', '[Ink]', '[Clement]', '[Michael]']),
-	new Meta (4, 400, 'Gossima VJ', '[Gossima VJ Cover]', '[Gossima VJ Description]', ['[MechBird]', '[Ben]', '[Leon]', '[Guillaume]']),
-	new Meta (4, 400, 'Gossima ping pong', '[Gossima Ping Pong Cover]', '[Gossima Ping Pong Description]')
 ], [
-	new Meta (3, defaultHeight, 'Brain Slash', '[Brain Slash Cover]', '[Brain Slash Description]', ['[Ben]', '[Leon]', '[Guillaume]']),
-	new Meta (3, defaultHeight, 'Audio Kaleido', '[Ben Kaleido Cover]', '[Ben Kaleido Description]', ['[Ben]']),
-	new Meta (3, defaultHeight, 'Ding Dong', '[Ding Dong Cover]', '[Ding Dong Description]', ['[Leon]', '[Ben]', '[Guillaume]']),
-	new Meta (3, defaultHeight, 'GPU', '[GPU Cover]', '', ['[MechBird]', '[Leon]'])
+	new Meta (6, defaultHeight, 'Gif Jam', '[Gif Jam Cover]', '[Gif Jam Description]', ['[Alexia]', '[Ben]', '[Ink]', '[Clement]', '[Michael]']),
+	new Meta (6, defaultHeight, 'Ding Dong', '[Ding Dong Cover]', '[Ding Dong Description]', ['[Leon]', '[Ben]', '[Guillaume]'])
 ], [
-	new Meta (4, defaultHeight, 'Splashes', '[Splashes Cover]', '', ['[Leon]']),
+	new Meta (5, defaultHeight, 'Audio Kaleido', '[Ben Kaleido Cover]', '[Ben Kaleido Description]', ['[Ben]']),
+	new Meta (4, defaultHeight, 'Gossima', '[Gossima Ping Pong Cover]', '[Gossima Ping Pong Description]'),
+	new Meta (3, defaultHeight, 'Brain Slash', '[Brain Slash Cover]', '[Brain Slash Description]', ['[Ben]', '[Leon]', '[Guillaume]'])
+], [
+	new Meta (4, defaultHeight, 'GPU', '[GPU Cover]', '', ['[MechBird]', '[Leon]']),
 	new Meta (4, defaultHeight, 'x?.3', '[X3 Cover]', '[X3 Description]', ['[MechBird]']),
 	new Meta (4, defaultHeight, 'Nivôse', '[Nivose Cover]', '[Nivose Description]', ['[MechBird]'])
 ], [
-	new Meta (4, defaultHeight, 'Warband', '[Warband Cover]', '', ['[Leon]']),
-	new Meta (4, defaultHeight, 'Glitch', '[Glitch1 Cover]', '', ['[Leon]']),
-	new Meta (4, defaultHeight, 'Morrowind', '[Morrowind Cover]', '', ['[Leon]'])
+	new Meta (3, defaultHeight, 'Splashes', '[Splashes Cover]', '', ['[Leon]']),
+	new Meta (3, defaultHeight, 'Glitch', '[Glitch2 Cover]', '', ['[Leon]']),
+	new Meta (3, defaultHeight, 'Warband', '[Warband Cover]', '', ['[Leon]']),
+	new Meta (3, defaultHeight, 'Morrowind', '[Morrowind Cover]', '', ['[Leon]'])
 ]];
